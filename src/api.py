e6571263143835486cd5fabca52f51feaff681cf
Robert <robert@MacBook-Pro-Robert.local>
2026-05-17 23:49:15 +0800

    Added fetching for community files

--- src/api.py
+++ src/api.py
@@ -17,7 +17,6 @@ def fetch_basic_repo_info(repo_path: str) -> dict:
     Fetches basic repository vitals: stars, forks, and open issues.
     """
     url = f"{BASE_URL}/repos/{repo_path}"
-
     response = requests.get(url, headers = HEADERS)
 
     if response.status_code == 404:
@@ -27,15 +26,35 @@ def fetch_basic_repo_info(repo_path: str) -> dict:
     
     if response.status_code == 200:
         data = response.json()
+    
+        community_url = f"{url}/community/profile"
+        community_response = requests.get(community_url, headers=HEADERS)
+
+        community_files = {
+            "has_readme": False,
+            "has_license": False,
+            "has_contributing": False,
+            "has_coc": False
+        }
+
+        if community_response.status_code == 200:
+            community_data = community_response.json()
+            community_files = {
+                "has_readme": bool(community_data.get("readme")),
+                "has_license": bool(community_data.get("license")),
+                "has_contributing": bool(community_data.get("contributing")),
+                "has_coc": bool(community_data.get("code_of_conduct"))
+            }
 
         repo_vitals = {
             "name": data.get("full_name"),
             "stars": data.get("stargazers_count"),
             "forks": data.get("forks_count"),
             "open_issues": data.get("open_issues_count"),
-            "last_pushed": data.get("pushed_at")
+            "last_pushed": data.get("pushed_at"),
+            "community": community_files
         }
-        
+
         return repo_vitals
     
     raise Exception(f"Unexpected error: {response.status_code}")
