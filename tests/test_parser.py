4a71cdf73bd3fdb8661c41ba511e38b0a84ce0f5
Robert <robert@192.168.0.14>
2026-05-18 19:16:02 +0800

    Renamed parser.py to avoid connection errors

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,5 +1,5 @@
 import pytest
-from src.parser import extract_repo_path
+from url_parser import extract_repo_path
 
 @pytest.mark.parametrize("input_url, expected", [
     ("https://github.com/facebook/react", "facebook/react"),
